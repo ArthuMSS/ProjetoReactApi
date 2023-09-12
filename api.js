424b6d90e50497d830463c450bf954babc0599bd
Arthur Santana <105656036+ArthuMSS@users.noreply.github.com>
2023-09-12 17:29:57 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '864f832d31msh8325f14896424a8p1f0724jsnf7b4f964853f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+    export const GEO_API_URL = "https://wtf-geo-db.p.rapidapi.com/v1/geo/cities";
+
